69f219e877e74be065285f0e37f3dbaee3279f09
Yuet Fu Tse <yuettse@hotmail.com>
2015-06-17 12:38:17 -0700

    Create pokemon1-151.php

---
+++ pokemon1-151.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Yuet Pokemon</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<? 
+	for($i = 1; $i < 152; $i++) {
+		echo "<img src ='http://pokeapi.co/media/img/" . $i . ".png'>";
+	}
+?>
+
+
+
+</body>
+</html>
